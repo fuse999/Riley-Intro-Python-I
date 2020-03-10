674e04c613ea6949889697f8946f03fa3b067766
riley pence <rileypence4@gmail.com>
2020-03-10 13:36:52 -0700

    Project 1 complete and in pep8

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
-
+with open('foo.txt') as f:
+    read_data = f.read()
+    print(read_data)
+    f.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+r = open('bar.txt', "w+")
+r.write('This is a test\n')
+r.write('This is a second test\n')
+r.write('This is a third test\n')
+r.close()
+
+with open('bar.txt') as f:
+    read_data = f.read()
+    print(read_data)
+    f.close()
--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,56 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
-from datetime import datetime
+from datetime import date, timedelta, datetime
+
+
+def days_cur_month(m=None, y=None):
+    if m == None:
+        m = datetime.now().month
+    else:
+        m = m
+    if y == None:
+        y = datetime.now().year
+    else:
+        y = y
+    ndays = (date(y, m+1, 1) - date(y, m, 1)).days
+    d1 = date(y, m, 1)
+    d2 = date(y, m, ndays)
+    delta = d2 - d1
+
+    return [(d1 + timedelta(days=i)).strftime('%Y-%m-%d') for i in range(delta.days + 1)]
+
+
+system_argument = sys.argv
+
+if len(system_argument) == 1:
+    print(days_cur_month())
+
+elif len(system_argument) == 2:
+    month_string = system_argument[1]
+    full_month_convert = datetime.strptime(
+        month_string, '%B').replace(year=2020)
+    month_digit = int(str(full_month_convert)[5:7])
+    print(days_cur_month(month_digit))
+
+elif len(system_argument) == 3:
+    month_string = system_argument[1]
+    full_month_convert = datetime.strptime(
+        month_string, '%B').replace(year=2020)
+    month_digit = int(str(full_month_convert)[5:7])
+    year = int(system_argument[2])
+    print(days_cur_month(month_digit, year))
+
+else:
+    print("Usage: script.py [month_name] [year]")
